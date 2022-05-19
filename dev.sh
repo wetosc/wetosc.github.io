99e29b935f4c110e8195bd57fd244f0576063ada
Eugeniu Cernei <eugen.cernei@gmail.com>
2022-05-19 03:40:41 +0300

    Squashed/rebased old commits into a single one

--- dev.sh
+++ dev.sh
@@ -2,7 +2,7 @@
 
 case $1 in
 	build)
-		docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" jekyll/jekyll:latest jekyll build
+		docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" jekyll/jekyll:latest jekyll build --trace
 		;;
 	serve)
 		docker run --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -p 4000:4000 -it jekyll/jekyll:latest jekyll serve --watch
